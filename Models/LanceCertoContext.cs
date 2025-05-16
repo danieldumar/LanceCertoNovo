77cb9977c7551429bca6cc41098cc11ced6e93d8
danieldumar <daniel.dumarques@gamil.com>
2025-05-16 13:41:23 -0300

    Adicionar arquivos de projeto.

---
+++ Models/LanceCertoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LanceCertoNovo.Models;
+
+namespace LanceCertoNovo.Data
+{
+    public class LanceCertoContext : DbContext
+    {
+        public LanceCertoContext(DbContextOptions<LanceCertoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Leilao> Leiloes { get; set; }
+    }
+}
+
---
+++ Models/Leilao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanceCertoNovo.Models
+{
+    public class Leilao
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal ValorInicial { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set; }
+    }
+}
+
